1fa48ef8a0947dc96c25414257d4b7fbc47eac3b
ossmcloud <development@ossmcloud.ie>
2023-06-16 13:08:25 +0100

    drop down paging issue fixed

--- render/render_base.js
+++ render/render_base.js
@@ -307,7 +307,10 @@ class RenderBase {
             }
         }
         if (renderType == _cxConst.RENDER.TYPE.DROP_DOWN) {
-            if (this.dataSource.count() == 0 && !this.options.noLoad) { await this.dataSource.select(options); }
+            if (this.dataSource.count() == 0 && !this.options.noLoad) {
+                options.noPaging = true;
+                await this.dataSource.select(options);
+            }
             await this.dropDown(options);
         }
         return this.#options;
